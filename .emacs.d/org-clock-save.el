15fdb3f4df48c4767a25c325c0e8cb83c5ee54a8
Ian Ross <ian@skybluetrades.net>
2018-11-14 13:13:04 +0100

    Tidying

--- .emacs.d/org-clock-save.el
+++
@@ -1,2 +0,0 @@
-;; org-clock-save.el - seneca at <2018-11-10 Sat 17:50>
-(setq org-clock-stored-history '(("/home/iross/org/memcachier.org" . 122189) ("/home/iross/org/memcachier.org" . 122781) ("/home/iross/org/memcachier.org" . 54096) ("/home/iross/org/memcachier.org" . 7025) ("/home/iross/org/memcachier.org" . 121592)))
